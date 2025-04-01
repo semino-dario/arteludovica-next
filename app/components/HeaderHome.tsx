0537109e1cc91f3da67f3b4b0ed132e2841a81e0
Darío Semino <dariosemino@mac.home>
2025-04-01 09:50:19 +0200

    add main title

--- app/components/HeaderHome.tsx
+++ app/components/HeaderHome.tsx
@@ -1,10 +1,10 @@
-import styles from './styles/page.module.scss';
+import styles from '../styles/page.module.scss';
 
 export default function HeaderHome () {
 
     return (
         <div>
-            
+            <h1 className={styles.mainTitle}>Compañía Arteludovica</h1>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./styles/page.module.scss";
+import HeaderHome from "./components/HeaderHome";
 
 export default function Home() {
   return (
     <div>
+      <HeaderHome/>
     </div>
   );
 }
