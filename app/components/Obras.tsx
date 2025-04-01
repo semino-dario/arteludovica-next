730c03c9a012faa9599ae1b1b6c1554c6c7b42a9
Darío Semino <dariosemino@mac.home>
2025-04-01 11:41:13 +0200

    add obras

---
+++ app/components/Obras.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/page.module.scss';
+
+export default function Obras() {
+    return(
+        <section className={styles.containerSection}>
+            <h1 className={styles.titleSection}>Obras</h1>
+            <p className={styles.textHome}>Podés entrar en cada una para recorrerla.
+
+</p>
+            <div></div>
+        </section>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
 import HeaderHome from "./components/HeaderHome";
+import Obras from "./components/Obras";
 
 export default function Home() {
   return (
     <div>
       <HeaderHome/>
+      <Obras/>
     </div>
   );
 }
