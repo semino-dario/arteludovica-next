d42fee68c0ff618b5235f98fedd8df2f97941a62
Darío Semino <dariosemino@mac.home>
2025-04-01 10:47:13 +0200

    finish home header

--- app/components/HeaderHome.tsx
+++ app/components/HeaderHome.tsx
@@ -8,6 +8,7 @@ export default function HeaderHome () {
             <h1 className={styles.mainTitle}>Compañía Arteludovica</h1>
             <h2 className={styles.mainSubtitle}>MEMORIAS DE UNA EXPERIANCIA TEATRAL</h2>
             <img className={styles.mainImage} src={mainImage.src} alt="Imagen de la Compañía de teatro" />
+            <p className={styles.textHome}>La Compañía Teatral Arteludovica estuvo activa entre 2006 y 2013 en Buenos Aires. Se puede seguir aquí el rastro de su trayectoria.</p>
         </div>
     )
 }
