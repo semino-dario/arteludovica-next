fda68b522068f6522af703779483c4b72cee5fa3
Darío Semino <dariosemino@mac.home>
2025-04-01 10:27:56 +0200

    add main Image

--- app/components/HeaderHome.tsx
+++ app/components/HeaderHome.tsx
@@ -1,10 +1,13 @@
 import styles from '../styles/page.module.scss';
+import mainImage from "../assets/images/home/mimoteatro-puerta.jpeg"
 
 export default function HeaderHome () {
 
     return (
-        <div>
+        <div className={styles.headerContainer}>
             <h1 className={styles.mainTitle}>Compañía Arteludovica</h1>
+            <h2 className={styles.mainSubtitle}>MEMORIAS DE UNA EXPERIANCIA TEATRAL</h2>
+            <img className={styles.mainImage} src={mainImage.src} alt="Imagen de la Compañía de teatro" />
         </div>
     )
 }
