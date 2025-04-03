13edf99772f46dda77c7f1c1575e6a19d17aef04
Darío Semino <dariosemino@mac.home>
2025-04-03 10:56:41 +0200

    finish textos

---
+++ app/assets/svg/PdfIcon.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import styles from "../../styles/page.module.scss";
+
+interface PdfIconProps {
+    pdfName: string;
+}
+
+const PdfIcon:React.FC<PdfIconProps> = ({pdfName}) => {
+
+    const [hover, setHover] = useState(false);
+    
+        const handlerMouseOver = () => {
+            setHover(true);
+        }
+    
+        const handlerMouseOut = () => {
+            setHover(false);
+        }
+
+    return(
+        <div>
+        <svg
+        onMouseOver={handlerMouseOver} onMouseOut={handlerMouseOut} 
+        className={styles.pointer}
+        width="99px" height="104px" viewBox="0 0 99 104" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        <g  id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g className="Group2" fill={ !hover ? "#E7DBC5" : "black"}>
+                <path className="icono-svg" d="M52.8865474,50.1177738 L71.5733631,67.3215089 C73.6049376,69.1918474 73.7356453,72.3549728 71.8653068,74.3865474 C70.9185725,75.4148968 69.5846027,76 68.1868157,76 L30.8131843,76 C28.0517605,76 25.8131843,73.7614237 25.8131843,71 C25.8131843,69.602213 26.3982875,68.2682433 27.4266369,67.3215089 L46.1134526,50.1177738 C48.027362,48.3557619 50.972638,48.3557619 52.8865474,50.1177738 Z" id="Triangle"  transform="translate(49.500000, 61.500000) scale(1, -1) translate(-49.500000, -61.500000) "></path>
+                <line x1="23.3375" y1="82.5" x2="76.6625" y2="82.5" id="Line-3" stroke="#000000" strokeWidth="5" strokeLinecap="square"></line>
+                <text id="PDF" fontFamily="AmericanTypewriter, American Typewriter" fontSize="21" fontWeight="normal" fill="#000000">
+                    <tspan x="28.668" y="35">PDF</tspan>
+                </text>
+            </g>
+        </g>
+    </svg>
+    <h3 className={styles.textHome}>{`${pdfName}.pdf`}</h3>
+    </div>
+    )
+}
+
+export default PdfIcon;
--- app/components/Textos.tsx
+++ app/components/Textos.tsx
@@ -1,26 +1,48 @@
 "use client";
 import { useState } from 'react';
 import styles from '../styles/page.module.scss';
+import CloseIcon from '../assets/svg/CloseIcon';
+import PdfIcon from '../assets/svg/PdfIcon';
 
 export default function Textos() {  
 
   const [showPdf, setShowPdf] = useState(false);
+  const [pdfName, setPdfName] = useState('');
 
-  const showPdfHandler = () => {
+  const showPdfHandler = (name:string) => {
     setShowPdf(true);
+    setPdfName(name);
+
+  }
+
+  const closePdfHandler = () => {
+    setShowPdf(false);
+    setPdfName('');
   }
     return (
         <section className={styles.containerSection}>
           <h1 className={styles.titleSection}>Textos</h1>
           <p className={styles.textHome}>Se pueden descargar en PDF los textos de las últimas dos obras estrenadas.</p>
          <div className={styles.containerTextos}>
-          <h2 onClick={showPdfHandler}>Las tejedoras</h2>
-          <h2 onClick={showPdfHandler}>El banquete</h2>
+          <h2 onClick={()=> showPdfHandler("texto-las-tejedoras")}
+              className={`${styles.pointer} ${pdfName === "texto-las-tejedoras" ? styles.active : ""}`}
+            >Las tejedoras</h2>
+          <h2 onClick={()=> showPdfHandler("texto-el-banquete")}
+              className={`${styles.pointer} ${pdfName === "texto-el-banquete" ? styles.active : ""}`}
+            >
+            El banquete</h2>
          </div>
          {
             showPdf &&
-          <div>
-            <h1>descargar pdf</h1>
+          <div className={styles.pdfContainer}>
+            <a href={`/pdf/${pdfName}.pdf`} target='_blank' rel='noopener noreferrer'>
+            <PdfIcon
+            pdfName={pdfName}
+            />
+            </a>
+            <CloseIcon
+              onClick={closePdfHandler}
+            />
           </div>}
         </section>
     )
